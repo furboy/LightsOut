d622753f3a7bf317ca3b199cb9c49b73f12a226c
furboy <46965862+furboy@users.noreply.github.com>
2019-05-13 08:43:07 -0500

    Add button toggles

--- app/src/main/java/com/example/lightsout/MainActivity.java
+++ app/src/main/java/com/example/lightsout/MainActivity.java
@@ -4,29 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button[] buttons;
+    Boolean[] State;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button buttons[] = new Button[9];
-        buttons[0] = (ToggleButton)findViewById(R.id.Button_1);
-        buttons[1] = (ToggleButton)findViewById(R.id.Button_2);
-        buttons[2] = (ToggleButton)findViewById(R.id.Button_3);
-        buttons[3] = (ToggleButton)findViewById(R.id.Button_4);
-        buttons[4] = (ToggleButton)findViewById(R.id.Button_5);
-        buttons[5] = (ToggleButton)findViewById(R.id.Button_6);
-        buttons[6] = (ToggleButton)findViewById(R.id.Button_7);
-        buttons[7] = (ToggleButton)findViewById(R.id.Button_8);
-        buttons[8] = (ToggleButton)findViewById(R.id.Button_9);
+        final Boolean state[] = new Boolean[9];
+        state[0] = false;
+        state[1] = false;
+        state[2] = false;
+        state[3] = false;
+        state[4] = false;
+        state[5] = false;
+        state[6] = false;
+        state[7] = false;
+        state[8] = false;
+
+
+
 
         final Button complete = findViewById(R.id.Finished);
 
@@ -35,12 +43,28 @@ public class MainActivity extends AppCompatActivity {
         complete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (buttons.toString() == "on"){
-                    Toast toast = Toast.makeText("A winner is you!",Toast.LENGTH_LONG).show();
+                if (Arrays. == "on"){
+                    Toast.makeText(MainActivity.this, "A winner is UwU", Toast.LENGTH_SHORT).show();
+
+                } else{
+                    Toast.makeText(MainActivity.this, "Try again", Toast.LENGTH_SHORT).show();
 
                 }
             }
         });
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.Button_1);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    //https://developer.android.com/guide/topics/ui/controls/togglebutton#java
+                } else {
+                    // The toggle is disabled
+                }
+            }
+        });
+
+
+
     }
 
 
